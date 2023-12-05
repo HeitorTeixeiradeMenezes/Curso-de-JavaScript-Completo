c6f0d3ce26e71feb87ea7a82f04c3c65423eccbd
Heitor <134107213+HeitorTeixeiradeMenezes@users.noreply.github.com>
2023-12-05 15:18:01 -0300

    IIFE

---
+++ 12-Funcoes2/1-IIFE/iife-menu.js
@@ -0,0 +1,16 @@
+// let isValid = false
+
+// function init(){
+//     console.log(`init menu`)
+// }
+ 
+
+(function(n1, n2){
+    let isValid = true
+    console.log(`menu`, isValid, n1, n2)
+
+    function init(){
+        console.log(`init do menu`)
+    }
+    init()
+})(20, 30)
---
+++ 12-Funcoes2/1-IIFE/iife-modal.js
@@ -0,0 +1,15 @@
+// let isValid = true
+
+// function init(){
+//     console.log(`init modal`)
+// }
+
+(function(){
+    let isValid = false
+    console.log(`modal`, isValid)
+
+    function init(){
+        console.log(`init do modal`)
+    }
+    init()
+})()
