bb6e8334a92b20fd733fad985e7b03dd09beecaf
Heitor <134107213+HeitorTeixeiradeMenezes@users.noreply.github.com>
2024-02-22 20:35:00 -0800

    toStringJoinConcat

---
+++ 14-ArrayProp/3-array.js
@@ -0,0 +1,9 @@
+arr1 = [1, 3, 15, 17, 30, 44, 48]
+arr2 = [28, 7, 30, 78, 43, 1, 23]
+
+console.log(arr1.toString())
+
+console.log(arr1.join(" "))
+
+arr3 = arr2.concat(arr1, "oi")
+console.log(arr3)
