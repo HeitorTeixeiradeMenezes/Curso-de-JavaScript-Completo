0ee27bb8e5f9a1bd362152c659e6268303256f9a
Heitor <134107213+HeitorTeixeiradeMenezes@users.noreply.github.com>
2024-06-28 12:18:21 -0300

    .

--- 18-listaTarefas/js/todolist.js
+++ 18-listaTarefas/js/todolist.js
@@ -12,22 +12,44 @@ let arrLis = [
 
 
 const createLi = (tarefa) =>{
-    lis = document.createElement("li")
+    const lis = document.createElement("li")
     lis.className = "todo-item"
-    button = document.createElement("button")
+    const button = document.createElement("button")
     button.className = "button-check"
     button.innerHTML = `<i class="fas fa-check displayNone"></i>`
-    p = document.createElement("p")
+    button.setAttribute("dataAction", "checkButton")
+    const p = document.createElement("p")
     p.className = tarefa
     p.textContent = tarefa
-    edit = document.createElement("i")
+    const edit = document.createElement("i")
     edit.className = "fas fa-edit"
-    trash = document.createElement("i")
+    edit.setAttribute("dataAction", "editButton")
+    const trash = document.createElement("i")
     trash.className = "fas fa-trash-alt"
+    trash.setAttribute("dataAction", "trashButton")
+
+    const containerEdit = document.createElement("div")
+    containerEdit.className = "editContainer"
+    const inputEdit = document.createElement("input")
+    inputEdit.setAttribute("type", "text")
+    inputEdit.className = "editInput"
+    containerEdit.appendChild(inputEdit)
+    const containerEditButton = document.createElement("button")
+    containerEditButton.className = "editButton"
+    containerEditButton.textContent = "Edit"
+    containerEditButton.setAttribute("dataAction", "containerEditButton")
+    containerEdit.appendChild(containerEditButton)
+    const containerCancelButton = document.createElement("button")
+    containerCancelButton.className = "cancelButton"
+    containerCancelButton.textContent = "Cancel"
+    containerCancelButton.setAttribute("dataAction", "containerCancelButton")
+    containerEdit.appendChild(containerCancelButton)
+
     lis.appendChild(button)
     lis.appendChild(p)
     lis.appendChild(edit)
     lis.appendChild(trash)
+    lis.appendChild(containerEdit)
     return lis
 }
 
@@ -48,17 +70,25 @@ const pushArrObj = function(tarefa){
 }
 
 
+todoList.addEventListener("click", function(e){
+    console.log(e.target.getAttribute("dataAction"))
+    console.log(e)
+})
+
+
 
 submit.addEventListener("click", function(e){
     e.preventDefault()
     if(newItem.value != ""){
     pushArrObj(newItem.value)
+    newItem.value = ""
     loadArrys()
     newItem.focus()
     } else{
         window.alert("preecha o Titulo")
     }
 })
-loadArrys()
 
-// Mudar o estilo do append child para ler os objs do Array
+
+
+loadArrys()
